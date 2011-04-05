a618e9d951309679a9c3f8c46631e666048aedf9
Erik Moen <eroen@eroen.eu>
2011-04-05 13:13:13 +0200

    Second session.

--- ce2/semesteroppgave_2.m
+++ ce2/semesteroppgave_2.m
@@ -3,7 +3,7 @@
 %
 % By Erik Moen, Geir Huth and Qingrui Zhou.
 
-%% Oppgave 1a
+%% Oppgave 1 a)
 % Impulse responses for the high- and lowpass channels.
 
 clear;
@@ -20,10 +20,10 @@ figure(); stem(imphp); xlabel('n'); ylabel('h_{hp}(n)');
 figure(); stem(implp); xlabel('n'); ylabel('h_{lp}(n)'); 
   title('Impulse response for the lowpass channel');
 
-%% Oppgave 1b
+%% Oppgave 1 b)
 % We show the impulses to be orthogonal at the required offsets, first visually.
 [Rhh,thp]=xcorr(imphp,imphp);
-[Rll,tlp]=xcorr(imphp,imphp);
+[Rll,tlp]=xcorr(implp,implp);
 [Rhl,thl]=xcorr(imphp,implp);
 
 figure(); stem(thp,Rhh); xlabel('\tau (lag)'); ylabel('R_{hh}(\tau)'); 
@@ -48,14 +48,14 @@ idx=sort([center+(2:2:(center-1)), center-(2:2:(center-1)), center]);
 max(abs(Rhl(idx)))
 
 
-%% Oppgave 1c
+%% Oppgave 1 c)
 % The frequency responses for the two channels are shown.
 figure(); freqz(imphp);
 title('Frequency response for highpass channel');
 figure(); freqz(implp);
 title('Frequency response for lowpass channel');
 
-%% Oppgave 1d
+%% Oppgave 1 d)
 % The variance over the channel is computed using two gaussian sources.
 N = 10000;
 
@@ -70,7 +70,7 @@ var(Yn1)
 %%
 % We observe the variance to be the same as the source signals, which makes sense as the rate is doubled.
 
-%% Oppgave 1e
+%% Oppgave 1 e)
 % The variance over the channel is computed using two binary sources.
 
 % Genererer binær gaussisk støy
@@ -84,7 +84,7 @@ var(Yn2)
 %%
 % We observe the variance to be the same as the source signals, which makes sense as the rate is doubled.
 
-%% Oppgave 1f
+%% Oppgave 1 f)
 % We will show that the input signals can be reconstructed perfectly by subtracting the reconstructed output from the input.
 %
 % Note that the modulation and demodulation creates an offset of 4 samples.
@@ -102,19 +102,8 @@ maxdiff_lpb = max( abs(En2b - Xn2b(5:N+4)) )
 %%
 % We attribute the differences to rounding errors.
 
-% [tx1g,Rx1g] = xcorr(En1g,Xn1g,25);
-% [tx2g,Rx2g] = xcorr(En2g,Xn2g,25);
-% [tx1b,Rx1b] = xcorr(En1b,Xn1b,25);
-% [tx2b,Rx2b] = xcorr(En2b,Xn2b,25);
-% 
-% figure(); stem(Rx1g,tx1g); xlabel('\tau (lag)'); ylabel('R_{xx}(\tau)');
-% figure(); stem(Rx2g,tx2b); xlabel('\tau (lag)'); ylabel('R_{xx}(\tau)');
-% figure(); stem(Rx1b,tx1b); xlabel('\tau (lag)'); ylabel('R_{xx}(\tau)');
-% figure(); stem(Rx2b,tx2b); xlabel('\tau (lag)'); ylabel('R_{xx}(\tau)');
 
-
-
-%% Oppgave 2a
+%% Oppgave 2 a)
 % The impulse and frequency responses for the channels in a 4-channel transmitter are drawn.
 close all;
 
@@ -142,7 +131,7 @@ title('Frequency response for channel 3');
 figure(); freqz(imp4);
 title('Frequency response for channel 4');
 
-%% Oppgave 2b
+%% Oppgave 2 b)
 % The orthogonality is tested.
 %
 % First, we  test the orthogonality of each channel against itself. As earlier, the requirement is that the channel is orthogonal for even offsets greater than 0.
@@ -171,7 +160,7 @@ max(max(abs(R(crosstestrow, crosscol))))
 
 defaultStream = RandStream.getDefaultStream();
 reset(defaultStream);
-L=1e4;
+L=1e6;
 x=randn(4, L);
 y=synN( synN(x(1,:), x(2,:), k), synN(x(3,:), x(4,:), k), k);
 %[zprime]=zeros(2, 20016);
@@ -191,7 +180,10 @@ max(max(x - z(:, 1+offset:L+offset)))
 % We observe only a very small number, and consider the signal uncorrupted.
 
 %% Problem 2 d)
-% In order to introduce noise to match a SNR given in dB, we must first convert the value to a number using the |db2pow| function. We can then calculate the variance (power) of the transmitted signal to obtain the correct noise variance or power.
+% In order to introduce noise to match a SNR given in dB, we must first 
+% convert the value to a number using the |db2pow| function. We can then 
+% calculate the variance (power) of the transmitted signal to obtain the 
+% correct noise variance or power.
 
 x=(randi(2, 4, L) * 2) - 3;
 y=synN( synN(x(1,:), x(2,:), k), synN(x(3,:), x(4,:), k), k);
@@ -199,41 +191,102 @@ n0=randn(size(y));
 S=var(y);
 
 SNR=db2pow(4);
-% We might have to divide this by 4.
 N=S/SNR;
-n=(N)*n0;
+n=(N/var(n0))*n0;
 ynoisy=y+n;
 
 %% Problem 2 e)
-% We will determine the performance of the system by testing it for bit error rate with different SNRs.
-snroffset=-30;
-snrscale=0.5;
-ival= 1:80;
+% We will determine the performance of the system by testing it for bit 
+% error rate with different SNRs.
+%
+% To compute the BER curve to a given BER, we use a length great enough for
+% the given BER to correspond to approximately 10 errors. In this case we
+% will calculate to |BER=10^-5|, and should use a length of |L=10^6|.
+%
+% The tested values for SNR were found experimentally.
+snrval=[-30:5:-10 -5:6 6.3 6.4];
+ival= 1:length(snrval);
 for i = ival
-	i
-    SNR=db2pow(i*snrscale+snroffset);
+    SNR=db2pow(snrval(i));
     N=S/SNR;
     n=N*n0;
     yn=y+n;
-    %[zprime]=zeros(2, 20016);
     [zprime(1,:), zprime(2,:)]=anaN(yn, k);
     [z(1,:), z(2,:)]=anaN(zprime(1,:), k);
     [z(3,:), z(4,:)]=anaN(zprime(2,:), k);
 
     zregen=zeros(size(z))-1;
     idx=find(z>0);
-    %Should address the issue of exact 0.
     zregen(idx)=1;
 
     ierr=find(not(x == zregen(:, offset+1:L+offset)));
     nerr=length(ierr);
     BER(i)=nerr/(L*4);
-    BER(i);
 end
 
-semilogy(ival*snrscale+snroffset, BER);
+
+%% Problem 2 f)
+% The BER curve is plotted below.
+
+semilogy(snrval, BER);
+title('Bit error rate, 4 channels');
+xlabel('SNR [dB]');
+ylabel('BER');
+
+%% Problem 2 g)
+% The experiment will be repeated to determine the BER curve for a
+% 8-channel system.
+clear zprime z BER x y yn n0
+
+x=(randi(2, 8, L) * 2) - 3;
+y=synN(synN( synN(x(1,:), x(2,:), k), synN(x(3,:), x(4,:), k), k), ... 
+       synN( synN(x(5,:), x(6,:), k), synN(x(7,:), x(8,:), k), k), k);
+n0=randn(size(y));
+S=var(y);
+
+snrval=fliplr([-30:5:-10 -5:6 6.3 6.4]);
+ival= 1:length(snrval);
+
+for i = ival
+    SNR=db2pow(snrval(i));
+    N=(S/var(n0))/SNR;
+    yn=y+N*n0;
+    [zprimeprime(1,:), zprimeprime(2,:)]=anaN(yn, k);
+
+    [zprime(1,:), zprime(2,:)]=anaN(zprimeprime(1,:), k);
+    [zprime(3,:), zprime(4,:)]=anaN(zprimeprime(2,:), k);
+    clear zprimeprime
+    [z(1,:), z(2,:)]=anaN(zprime(1,:), k);
+    [z(3,:), z(4,:)]=anaN(zprime(2,:), k);
+    [z(5,:), z(6,:)]=anaN(zprime(3,:), k);
+    [z(7,:), z(8,:)]=anaN(zprime(4,:), k);
+    clear zprime
+    if i==1
+        offset=find(xcorr(x(1,:), z(1,:))>=L/2)-L;
+    end
+
+    zregen=zeros(size(z))-1;
+    idx=find(z>0);
+    zregen(idx)=1;
+
+    ierr=find(not(x == zregen(:, offset+1:L+offset)));
+    nerr=length(ierr);
+    BER(i)=nerr/(L*8);
+end
+
+semilogy(snrval, BER);
+title('Bit error rate, 8 channels');
+xlabel('SNR [dB]');
+ylabel('BER');
 
 
+%% Problem 2 h)
+% We find the BER curve to be close to or equal for 4 and 8 channels. We
+% conclude that the increase of throughput only affects bandwidth. 
+%
+% The given building blocks could be incorporated into a system between the
+% forward error correcting coder and the modulator. (Vice versa on the
+% reciever side)
 
 snapnow;
 close all;
